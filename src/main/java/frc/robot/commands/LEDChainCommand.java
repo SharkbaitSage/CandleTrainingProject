f7686ca0e3438926991565682613e5202d2f43e7
SharkbaitSage <fulcher.bennett.00@gmail.com>
2025-11-20 12:05:38 -0500

    made the program create pretty rainbows

--- src/main/java/frc/robot/commands/LEDChainCommand.java
+++ src/main/java/frc/robot/commands/LEDChainCommand.java
@@ -4,15 +4,15 @@ import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.lights.LEDlights;
 import frc.robot.subsystems.lights.LEDlights.Colour;
+import frc.robot.subsystems.lights.LEDlights.CommonColours;
 
 public class LEDChainCommand extends Command {
     private final LEDlights ledSubsystem; 
     private final Timer timer = new Timer();
     private final Timer secondTimer = new Timer();
     private int indexAmount = 1;
-    private final Colour OFF = new Colour (0,0,0);
-    private final Colour CHARTREUSE = new Colour(223,255,0);
-    private final Colour RED = new Colour(255,0,0);
+    Colour randomColour;
+    
     public LEDChainCommand (LEDlights ledSubsystem) {
         this.ledSubsystem = ledSubsystem;
         addRequirements(ledSubsystem);
@@ -25,20 +25,20 @@ public class LEDChainCommand extends Command {
         secondTimer.start();
         timer.reset();
         secondTimer.reset();
-        indexAmount=-1;
+        indexAmount = -1;
     }
     public void execute() {
         int turnOffLights = indexAmount - 10;
         if (turnOffLights < 0) {
             turnOffLights = 0;
         }
-        if (timer.get() > 0.125 ) {
+        if (timer.get() > 0.1 ) {
             indexAmount++;
             timer.reset();
-            ledSubsystem.setLEDS(CHARTREUSE,indexAmount,10);
+            ledSubsystem.setLEDS(CommonColours.CHARTREUSE.colour, indexAmount, 10);
         }
         else if (secondTimer.get() > 0.25 ) {
-            ledSubsystem.setLEDS(RED, turnOffLights,10);
+            ledSubsystem.setLEDS(randomColour, turnOffLights, 10);
             secondTimer.reset();
         }
 
@@ -46,5 +46,17 @@ public class LEDChainCommand extends Command {
         if (indexAmount >= 212) {
             indexAmount = -1;
         }
+
+        
+        int randomColourIndex = (int) (Math.random() * 6);
+        randomColour = switch (randomColourIndex) {
+            case 0 -> CommonColours.BLUE.colour;
+            case 1 -> CommonColours.CYAN.colour;
+            case 2 -> CommonColours.RED.colour;
+            case 3 -> CommonColours.GREEN.colour;
+            case 4 -> CommonColours.PURPLE.colour;
+            case 5 -> CommonColours.YELLOW.colour;
+            default -> CommonColours.OFF.colour;
+        };
     }
 }
